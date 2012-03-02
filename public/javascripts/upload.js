f6dd0ddad604fb78fa51e945cb3cc22325a297c1
Freek Kalter <freek@kalteronline.org>
2012-03-02 20:57:35 +0100

    upload progress works again in firefox
    - chrome has stil a problem problably with xmlhttprequest

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -1,33 +1,30 @@
  interval = null;
 
 function openProgressBar() {
- /* generate random progress-id */
- uuid = "";
- for (i = 0; i < 32; i++) {
-  uuid += Math.floor(Math.random() * 16).toString(16);
- }
- /* patch the form-action tag to include the progress-id */
- document.getElementById("upload-form").action="/upload?X-Progress-ID=" + uuid;
- console.log(uuid);
+    /* generate random progress-id */
+    uuid = "";
+    for (i = 0; i < 32; i++) {
+     uuid += Math.floor(Math.random() * 16).toString(16);
+    }
+    /* patch the form-action tag to include the progress-id */
+    console.log('net gemaakt: ' + uuid);
+    document.getElementById("upload-form").action="/upload?X-Progress-ID=" + uuid;
 
- $('#progress').slideDown('slow');
- $('img.ajax-loader').fadeIn('slow');
- /* call the progress-updater every 1000ms */
- interval = window.setInterval(
-   function () {
-     fetch(uuid);
-   },
-   5000
- );
+    $('#progress').slideDown('slow');
+    $('img.ajax-loader').fadeIn('slow');
+    /* call the progress-updater every 1000ms */
+    interval = window.setInterval(
+      function () {
+        fetch(uuid);
+      },
+      5000
+    );
 }
 
 function fetch(uuid) {
- var bar_width = 300;
- req = new XMLHttpRequest();
- req.open("GET", "/progress", 1);
- req.setRequestHeader("X-Progress-ID", uuid);
- console.log(uuid);
+ var req = createInstance();
  req.onreadystatechange = function () {
+    console.log('ready state change (' +  req.readyState + ')');
   if (req.readyState == 4) {
    if (req.status == 200) {
     /* poor-man JSON parser */
@@ -36,7 +33,7 @@ function fetch(uuid) {
     document.getElementById('tp').innerHTML = upload.state;
 
     /* change the width if the inner progress-bar */
-    if (upload.state == 'done' || upload.state == 'uploading') {
+    if (upload.state == 'uploading') {
      bar = document.getElementById('progressbar');
      w = bar_width * upload.received / upload.size;
      bar.style.width = w + 'px';
@@ -45,12 +42,24 @@ function fetch(uuid) {
     if (upload.state == 'done') {
        bar.style.width = bar_width;
         window.clearTimeout(interval);
-        setTimeout("window.location.href = '/me/shared'", 2000);
+//        setTimeout("window.location.href = '/me/shared'", 20000);
     }
    }
   }
  }
- req.send(null);
+
+ var bar_width = 300;
+ console.log('req.open: ' + req.open("GET", "/progress", true));
+ req.setRequestHeader("X-Progress-ID", uuid);
+ console.log('req.send ' + uuid + ': ' + req.send(null));
+}
+
+function createInstance(){
+   if(window.XMLHttpRequest){
+      req = new XMLHttpRequest();
+   }else
+      aler('Kut');
+   return req;
 }
 
 function trim(value) {
